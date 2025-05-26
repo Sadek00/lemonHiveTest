329ae0c2dc7d96725ecc17257be539709cf056a5
Sadekur Rahman <48727846+Sadek00@users.noreply.github.com>
2025-05-26 16:28:51 +0600

    Add EFCore

---
+++ EBazaarWebApi/Data/Context/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using EBazaarWebApi.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace EBazaarWebApi.Data.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        // Define your DbSets (tables) here
+        public DbSet<Product> Products { get; set; }
+    }
+}
--- EBazaarWebApi/Program.cs
+++ EBazaarWebApi/Program.cs
@@ -1,12 +1,28 @@
+using EBazaarWebApi.Data.Context;
+using EBazaarWebApi.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Add Swagger services
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+
+// Enable Swagger in development mode
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.UseHttpsRedirection();
 
